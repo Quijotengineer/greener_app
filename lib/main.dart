460384379421afdefd2fb4092ee15f7f4c204005
Quijotengineer <inigo.monreal@bath.edu>
2023-02-12 12:51:16 +0100

    Added user icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(greenerApp());
+}
+
+class greenerApp extends StatelessWidget {
+  //const greenerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Green Dashboard Overview'),
-          backgroundColor: Colors.green[800],
+          backgroundColor: Colors.teal[900],
         ),
-        backgroundColor: Colors.green[600],
-        body: Center(
-          child: Image(
-            image: AssetImage('images/footprint.png'),
-          ),
+        backgroundColor: Colors.teal[500],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/footprint.png'),
+                ),
+              Text(
+                'Aitor Monreal',
+                  style: TextStyle(
+                    color: Colors.teal[50],
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  )
+              ),
+
+              Container(
+                height: 400.0,
+                width: 400.0,
+                margin: EdgeInsets.all(20.0), // for outside of widget
+                padding: EdgeInsets.all(20.0), // for inside of widget
+                color: Colors.teal[100],
+                child: Image(
+                  image: AssetImage('images/footprint.png'),
+                ),
+              ),
+              Container(
+                height: 10.0,
+                width: 10.0,
+                margin: EdgeInsets.all(20.0), // for outside of widget
+                padding: EdgeInsets.all(20.0), // for inside of widget
+                color: Colors.teal[100],
+                child: Image(
+                  image: AssetImage('images/footprint.png'),
+                ),
+              ),
+            ],
+          )
         ),
       ),
-    ),
-  );
+    );
+  }
 }
