a31e6478a7a0745734a680ad940c7da44b0e08da
Quijotengineer <inigo.monreal@bath.edu>
2023-02-12 22:08:52 +0100

    Added compost icon

--- lib/main.dart
+++ lib/main.dart
@@ -17,47 +17,69 @@ class greenerApp extends StatelessWidget {
         ),
         backgroundColor: Colors.teal[500],
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/footprint.png'),
-                ),
-              Text(
-                'Aitor Monreal',
-                  style: TextStyle(
-                    fontFamily: 'Exo2',
-                    color: Colors.teal[50],
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
-                  )
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/footprint.png'),
+            ),
+            Text(
+              'Aitor Monreal',
+              style: TextStyle(
+                fontFamily: 'Exo2',
+                color: Colors.teal[50],
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
               ),
-
-              Container(
-                height: 100.0,
-                width: 100.0,
-                margin: EdgeInsets.all(20.0), // for outside of widget
-                padding: EdgeInsets.all(20.0), // for inside of widget
-                color: Colors.teal[100],
-                child: Image(
-                  image: AssetImage('images/footprint.png'),
+            ),
+            Container(
+              color: Colors.teal[50],
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                //child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.compost,
+                      size: 100.0,
+                      color: Colors.teal[900],
+                    ),
+                    Text(
+                      'Profile',
+                      style: TextStyle(
+                        fontFamily: 'Exo2',
+                        color: Colors.teal[500],
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                margin: EdgeInsets.all(20.0), // for outside of widget
-                padding: EdgeInsets.all(20.0), // for inside of widget
-                color: Colors.teal[100],
-                child: Image(
-                  image: AssetImage('images/footprint.png'),
-                ),
+            //),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              margin: EdgeInsets.all(20.0), // for outside of widget
+              padding: EdgeInsets.all(20.0), // for inside of widget
+              color: Colors.teal[100],
+              child: Image(
+                image: AssetImage('images/footprint.png'),
               ),
-            ],
-          )
-        ),
+            ),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              margin: EdgeInsets.all(20.0), // for outside of widget
+              padding: EdgeInsets.all(20.0), // for inside of widget
+              color: Colors.teal[100],
+              child: Image(
+                image: AssetImage('images/footprint.png'),
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
